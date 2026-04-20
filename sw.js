4efc9239afae51cb4c34328cb4135c13e34fde21
pinheiro767 <claudiapinheiro767@gmail.com>
2026-04-20 03:12:21 -0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "atlas-dorso-torax-v2";
+
+const URLS_TO_CACHE = [
+  "/",
+  "/manifest.json",
+  "/static/css/style.css",
+  "/static/js/app.js",
+  "/static/js/three3d.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
